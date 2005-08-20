09b38d262138b183c697221899b59f856e9acc26
Geoffrey Ely <gely@usc.edu>
2005-08-19 18:54:39 -0700

    .

--- fortran/output.f95
+++ fortran/output.f95
@@ -20,6 +20,8 @@ do iz = 1, nout
   case('x'); nc = 3
   case('u'); nc = 3
   case('v'); nc = 3
+  case('uslip'); nc = 1
+  case('vslip'); nc = 1
   case default; print '(a)', 'outvar ' // outvar(iz); stop
   end select
   do i = 1, nc
@@ -39,16 +41,18 @@ end if
 if ( doit ) then
   call zoneselect( i1, i2, iout(iz,:), npg, hypocenter, nrmdim )
   if ( any( i1 < i1node .or. i2 > i2node .or. i2 < i1 ) ) stop 'output error'
-  reclen = floatsize * product( i2 - i1 + 1 )
-  j1 = i1(1); j2 = i2(1)
-  k1 = i1(2); k2 = i2(2)
-  l1 = i1(3); l2 = i2(3)
   select case ( outvar(iz) )
   case('x'); nc = 3
   case('u'); nc = 3
   case('v'); nc = 3
+  case('uslip'); nc = 1; i1(nrmdim) = 1; i2(nrmdim) = 1
+  case('vslip'); nc = 1; i1(nrmdim) = 1; i2(nrmdim) = 1
   case default; print '(a)', 'outvar ' // outvar(iz); stop
   end select
+  reclen = floatsize * product( i2 - i1 + 1 )
+  j1 = i1(1); j2 = i2(1)
+  k1 = i1(2); k2 = i2(2)
+  l1 = i1(3); l2 = i2(3)
   do i = 1, nc
     write( ofile, '(a,i2.2,a,i1,a,i5.5)' ) 'out/', iz, '/', i, '/', it
     open( 9, file=ofile, form='unformatted', access='direct', recl=reclen )
@@ -56,6 +60,8 @@ if ( doit ) then
     case('x'); write( 9, rec=1 ) x(j1:j2,k1:k2,l1:l2,i)
     case('u'); write( 9, rec=1 ) u(j1:j2,k1:k2,l1:l2,i)
     case('v'); write( 9, rec=1 ) v(j1:j2,k1:k2,l1:l2,i)
+    case('uslip'); write( 9, rec=1 ) uslip(j1:j2,k1:k2,l1:l2,i)
+    case('vslip'); write( 9, rec=1 ) vslip(j1:j2,k1:k2,l1:l2,i)
     case default; print '(a)', 'outvar ' // outvar(iz); stop
     end select
     close( 9 )
