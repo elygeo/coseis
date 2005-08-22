5d1ff3e834346031e54dfbefb5051522a4ea06c6
Geoffrey Ely <gely@usc.edu>
2005-08-22 16:52:10 -0700

    .

--- fortran/main.f95
+++ fortran/main.f95
@@ -5,7 +5,7 @@ use globals
 
 implicit none
 integer :: wt(3), wt_rate
-real :: dwt(3)
+real :: dwt(2)
 
 print '(a)', 'SORD - Support Opperator Rupture Dynamics'
 call system_clock( count_rate=wt_rate )
@@ -17,7 +17,7 @@ call output
 
 print '(a)', 'Main time loop'
 print '(a)', 'Step     V        U        W       I/O    Total'
-print '(a)', 'Iter  Compute  In/Out   Elapsed  |U|max   |V|max'
+print '(a)', 'Iter  Compute  In/Out   |U|max   |V|max'
 
 it = 0
 
@@ -30,7 +30,7 @@ do while ( it < nt )
   call system_clock( wt(2) )
   call output
   call system_clock( wt(3) )
-  dwt = real( (/ wt(2:3) - wt(1:2), wt(3) /) ) / real( wt_rate )
+  dwt = real( wt(2:3) - wt(1:2) ) / real( wt_rate )
   print '(i4,x,5(e9.2))', it, dwt, umax, vmax
 end do
 
