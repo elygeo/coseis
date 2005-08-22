6be13cee052012ad4483e2c1e7594614d1781ba0
Geoffrey Ely <gely@usc.edu>
2005-08-22 16:45:56 -0700

    .

--- fortran/main.f95
+++ fortran/main.f95
@@ -4,12 +4,11 @@ program sord
 use globals
 
 implicit none
-integer :: wt(5), wt_rate
+integer :: wt(3), wt_rate
 real :: dwt(3)
 
 print '(a)', 'SORD - Support Opperator Rupture Dynamics'
-call system_clock( wt(1), count_rate=wt_rate )
-print *, wt(1)
+call system_clock( count_rate=wt_rate )
 call inputs
 call gridgen
 call matmodel
@@ -24,15 +23,14 @@ it = 0
 
 do while ( it < nt )
   it = it + 1;
-  call system_clock( wt(2) )
+  call system_clock( wt(1) )
   call vstep
   call ustep
   call wstep
-  call system_clock( wt(3) )
+  call system_clock( wt(2) )
   call output
-  call system_clock( wt(4) )
-  dwt(1:2) = real( wt(3:4) - wt(2:3) ) / real( wt_rate )
-  dwt(3)   = real( wt(4)   - wt(1)   ) / real( wt_rate )
+  call system_clock( wt(3) )
+  dwt(1:2) = real( (/ wt(2:3) - wt(1:2), wt(3) /) ) / real( wt_rate )
   print '(i4,x,5(e9.2))', it, dwt, umax, vmax
 end do
 
