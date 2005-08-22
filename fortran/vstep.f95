32146fbec91f1903407d187c94ef47cf8998a8ad
Geoffrey Ely <gely@usc.edu>
2005-08-22 10:32:15 -0700

    .

--- fortran/vstep.f95
+++ fortran/vstep.f95
@@ -18,6 +18,7 @@ if ( verb > 1 ) print '(a)', 'Vstep'
 s2 = 0.
 outer: do ic  = 1, 3
 inner: do iid = 1, 3
+print *, '111', ic, iid
   id = mod( ic + iid - 1, 3 ) + 1
   ix = 6 - ic - id
   do iz = 1, noper
@@ -25,10 +26,10 @@ inner: do iid = 1, 3
     i1 = max( i1, i1node )
     i2 = min( i2, i2node )
     if ( ic == id ) then
-      print *, 'xx', ic, id
+print *, '222', ic, id
       call dfcn( s2, oper(iz), w1, x, dx, ic, id, i1, i2 )
     else
-      print *, 'xy', ix, id
+print *, '333', ix, id
       call dfcn( s2, oper(iz), w2, x, dx, ix, id, i1, i2 )
     end if
   end do
