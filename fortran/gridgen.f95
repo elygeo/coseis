ad79d58d75f076941cc2115f761424f1fb3e9f0a
Geoffrey Ely <gely@usc.edu>
2005-08-22 16:33:26 -0700

    .

--- fortran/gridgen.f95
+++ fortran/gridgen.f95
@@ -17,13 +17,13 @@ k1 = i1(2); k2 = i2(2)
 l1 = i1(3); l2 = i2(3)
 print *, i1, i2
 allocate( &
+   s1(j1:j2,k1:k2,l1:l2), &
+   s2(j1:j2,k1:k2,l1:l2), &
     x(j1:j2,k1:k2,l1:l2,3), &
     v(j1:j2,k1:k2,l1:l2,3), &
     u(j1:j2,k1:k2,l1:l2,3), &
    w1(j1:j2,k1:k2,l1:l2,3), &
-   w2(j1:j2,k1:k2,l1:l2,3), &
-   s1(j1:j2,k1:k2,l1:l2), &
-   s2(j1:j2,k1:k2,l1:l2) &
+   w2(j1:j2,k1:k2,l1:l2,3) &
  )
 x = 0.
 forall( i=j1:j2 ) x(i,:,:,1) = i - 1
