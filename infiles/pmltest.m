b1ea2e9922d97aacc9e222a7013125224cfc892f
Geoffrey Ely <gely@usc.edu>
2005-09-23 12:13:17 -0700

    .

--- infiles/pmltest.m
+++ infiles/pmltest.m
@@ -1,16 +1,14 @@
 %------------------------------------------------------------------------------%
-% Test PML boundary conditions
 
+  model		= 'pmltest'			;
   nn		= [ 81 81 81 ]			;
   nn		= [ 41 41 41 ]			;
-
+  ihypo		= [ 21 21 21 ]			;
   bc		= [ 1 1 1   1 1 1 ]		;
   bc		= [ 1 1 1   0 0 0 ]		;
-
-  faultnormal	= 0				;
-  ihypo		= [ 21 21 21 ]			;
-  rsource	= 150.				;
   nt		= 200				;
+  faultnormal	= 0				;
+  rsource	= 150.				;
   out		= { 'x' 1   1 1 0    41 41 0  }	;
   out		= { 'v' 1   1 1 0    41 41 0  }	;
   out		= { 'x' 1   1 1 31   41 41 31 }	;
