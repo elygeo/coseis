15f2d0e04ef2fc651019f3af12ffc413f331d61a
Geoffrey Ely <gely@usc.edu>
2006-05-10 20:21:09 -0700

    .

--- version.m
+++ version.m
@@ -1,5 +1,5 @@
-version = '2006-05-10-19-15-16';
-date    = 'Wed May 10 19:15:16 PDT 2006';
+version = '2006-05-10-20-21-09';
+date    = 'Wed May 10 20:21:09 PDT 2006';
 host    = 'phat';
 dir     = '/space/gely/sord';
 os      = 'Linux phat 2.6.16-1.2111_FC5 #1 Thu May 4 21:16:58 EDT 2006 i686 i686 i386 GNU/Linux';
