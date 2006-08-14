b04029b9de1fd2a53d56e7ee8a773b30b4c851f7
Geoffrey Ely <gely@usc.edu>
2006-08-13 17:41:49 -0700

    .

--- util/tsgrid.f90
+++ util/tsgrid.f90
@@ -2,20 +2,28 @@
 program grid
 use m_tscoords
 implicit none
-real :: r, dx, h, o1, o2, z0, h1, h2, h3, h4, ell(3), x0, y0, xf(6), yf(6), rf(6), zf, exag
+real :: r, dx, h, o1, o2, r1, r2, h1, h2, h3, h4, ell(3), x0, y0, z0, xf(6), yf(6), rf(6), zf, exag
 integer :: n(3), nn, npml, nrect, i, j, k, l, j1, k1, l1, j2, k2, l2, jf0, kf0, lf0, &
   nf, nf1, nf2, nf3, reclen
 real, allocatable :: x(:,:,:,:), w(:,:,:,:), s(:,:,:), t(:,:)
 character :: endian
 
+! Model parameters
 open( 1, file='dx', status='old' )
 read( 1, * ) dx
 close( 1 )
+ell = (/ 600, 300, 80 /) * 1000
+exag = 1.
+npml = 10
+xf = (/ 265864.,293831.,338482.,364062.,390075.,459348. /)
+yf = (/ 183273.,187115.,200421.,212782.,215126.,210481. /)
+zf = 16000.
+x0 = .5 * ( minval(xf) + maxval(xf) )
+y0 = .5 * ( minval(yf) + maxval(yf) )
+!x0 = 365000.
+!y0 = 202000.
 
 ! Dimentions
-npml = 10
-exag = 1.
-ell = (/ 600, 300, 80 /) * 1000
 n = nint( ell / dx ) + 1
 print *, 'n =', n
 j = n(1)
@@ -31,17 +39,8 @@ close( 1 )
 forall( i=1:n(1) ) x(i,:,:,1) = dx*(i-1)
 forall( i=1:n(2) ) x(:,i,:,2) = dx*(i-1)
 
-! Fault coordinates
-xf = (/ 265864.,293831.,338482.,364062.,390075.,459348. /)
-yf = (/ 183273.,187115.,200421.,212782.,215126.,210481. /)
-zf = 16000.
-!x0 = 365000.
-!y0 = 202000.
-x0 = .5 * ( minval(xf) + maxval(xf) )
-y0 = .5 * ( minval(yf) + maxval(yf) )
-nf = size( xf, 1 )
-
 ! Fault length
+nf = size( xf, 1 )
 rf(1) = 0
 do i = 2, nf
   h1 = xf(i) - xf(i-1)
@@ -114,6 +113,13 @@ forall( k=k1+1:k2-1 )
   x(:,k,:,:) = x(:,k1,:,:)*(k2-k)/(k2-k1) + x(:,k2,:,:)*(k-k1)/(k2-k1)
 end forall
 
+! lon/lat
+w = x
+call ts2ll( w, 1, 2 )
+if ( any( w /= w ) ) stop 'NaNs in lon/lat'
+print *, 'longitude range: ', minval( w(:,:,:,1) ), maxval( w(:,:,:,1) )
+print *, 'latgitude range: ', minval( w(:,:,:,2) ), maxval( w(:,:,:,2) )
+
 ! Topo
 allocate( t(960,780) )
 endian = 'l'
@@ -123,68 +129,19 @@ open( 1, file='topo.'//endian, recl=reclen, form='unformatted', access='direct',
 read( 1, rec=1 ) t
 close( 1 )
 t = t * exag
-
-! 2D x/y
-inquire( iolength=reclen ) x(:,:,:,1)
-open( 1, file='x', recl=reclen, form='unformatted', access='direct' )
-open( 2, file='y', recl=reclen, form='unformatted', access='direct' )
-write( 1, rec=1 ) x(:,:,:,1)
-write( 2, rec=1 ) x(:,:,:,2)
-close( 1 )
-close( 2 )
-
-! 3D x/y
-inquire( iolength=reclen ) x(:,:,:,1)
-open( 1, file='x1', recl=reclen, form='unformatted', access='direct' )
-open( 2, file='x2', recl=reclen, form='unformatted', access='direct' )
-do i = 1, n(3)
-  write( 1, rec=i ) x(:,:,:,1)
-  write( 2, rec=i ) x(:,:,:,2)
-end do
-close( 1 )
-close( 2 )
-
-! lat/lon
-w = x
-call ts2ll( w, 1, 2 )
-if ( any( w /= w ) ) stop 'NaNs in lon/lat'
-print *, 'longitude range: ', minval( w(:,:,:,1) ), maxval( w(:,:,:,1) )
-print *, 'latgitude range: ', minval( w(:,:,:,2) ), maxval( w(:,:,:,2) )
-
-! PML regions are orthogonal
-j = n(1)
-k = n(2)
-do i = npml-1,0,-1
-  w(i+1,:,:,:) = w(i+2,:,:,:)
-  w(j-i,:,:,:) = w(j-i-1,:,:,:)
-  w(:,i+1,:,:) = w(:,i+2,:,:)
-  w(:,k-i,:,:) = w(:,k-i-1,:,:)
-end do
-
-! 3D lat/lon
-open( 1, file='rlon', recl=reclen, form='unformatted', access='direct' )
-open( 2, file='rlat', recl=reclen, form='unformatted', access='direct' )
-do i = 1, n(3)
-  write( 1, rec=i ) w(:,:,:,1)
-  write( 2, rec=i ) w(:,:,:,2)
-end do
-close( 1 )
-close( 2 )
-
-! Topo
 o1 = 15. - 121.5 * 3600.
 o2 = 15. +  30.5 * 3600.
 h  = 30.
-w(:,:,:,1) = ( ( w(:,:,:,1) * 3600 ) - o1 ) / h
-w(:,:,:,2) = ( ( w(:,:,:,2) * 3600 ) - o2 ) / h
-do k = 1, size(x,2)
-do j = 1, size(x,1)
-  j1 = int( w(j,k,1,1) ) + 1
-  k1 = int( w(j,k,1,2) ) + 1
-  h1 =  w(j,k,1,1) - j1 + 1
-  h2 = -w(j,k,1,1) + j1
-  h3 =  w(j,k,1,2) - k1 + 1
-  h4 = -w(j,k,1,2) + k1
+do k = 1, size(w,2)
+do j = 1, size(w,1)
+  r1 = ( ( w(:,:,:,1) * 3600 ) - o1 ) / h
+  r2 = ( ( w(:,:,:,2) * 3600 ) - o2 ) / h
+  j1 = int( r1 ) + 1
+  k1 = int( r2 ) + 1
+  h1 =  r1 - j1 + 1
+  h2 = -r1 + j1
+  h3 =  r2 - k1 + 1
+  h4 = -r2 + k1
   x(j,k,1,3) = ( &
     h2 * h4 * t(j1,k1)   + &
     h1 * h4 * t(j1+1,k1) + &
@@ -195,31 +152,67 @@ end do
 z0 = sum( x(:,:,:,3) ) / ( n(1) * n(2) )
 print *, 'elevation range: ', minval( x(:,:,:,3) ), maxval( x(:,:,:,3) )
 
-! 2D elevation
+! 2D files
+inquire( iolength=reclen ) x(:,:,:,1)
+open( 1, file='x', recl=reclen, form='unformatted', access='direct' )
+open( 2, file='y', recl=reclen, form='unformatted', access='direct' )
 open( 3, file='z', recl=reclen, form='unformatted', access='direct' )
+write( 1, rec=1 ) x(:,:,:,1)
+write( 2, rec=1 ) x(:,:,:,2)
 write( 3, rec=1 ) x(:,:,:,3)
+close( 1 )
+close( 2 )
 close( 3 )
 
-! 3D elevation and depth. blend topo at fault bottom to flat basement
-s = 0
+! PML regions are orthogonal
+j = n(1)
+k = n(2)
+do i = npml-1,0,-1
+  t(i+1,:,:,3) = t(i+2,:,:,3)
+  t(j-i,:,:,3) = t(j-i-1,:,:,3)
+  t(:,i+1,:,3) = t(:,i+2,:,3)
+  t(:,k-i,:,3) = t(:,k-i-1,:,3)
+  w(i+1,:,:,:) = w(i+2,:,:,:)
+  w(j-i,:,:,:) = w(j-i-1,:,:,:)
+  w(:,i+1,:,:) = w(:,i+2,:,:)
+  w(:,k-i,:,:) = w(:,k-i-1,:,:)
+end do
+
+! 3D files
+inquire( iolength=reclen ) x(:,:,:,1)
+open( 1, file='x1', recl=reclen, form='unformatted', access='direct' )
+open( 2, file='x2', recl=reclen, form='unformatted', access='direct' )
 open( 3, file='x3', recl=reclen, form='unformatted', access='direct' )
-open( 4, file='rdep', recl=reclen, form='unformatted', access='direct' )
+open( 7, file='rlon', recl=reclen, form='unformatted', access='direct' )
+open( 8, file='rlat', recl=reclen, form='unformatted', access='direct' )
+open( 9, file='rdep', recl=reclen, form='unformatted', access='direct' )
+do l = 1, n(3)
+  write( 1, rec=l ) x(:,:,:,1)
+  write( 2, rec=l ) x(:,:,:,2)
+  write( 7, rec=l ) w(:,:,:,1)
+  write( 8, rec=l ) w(:,:,:,2)
+end do
+s = 0
 l1 = npml + 1
 l2 = n(3) - nf3
 do l = 1, l1
   write( 3, rec=l ) -dx*(n(3)-l) + z0 + s
-  write( 4, rec=l )  dx*(n(3)-l1) - z0 + x(:,:,:,3)
+  write( 9, rec=l )  dx*(n(3)-l1) - z0 + x(:,:,:,3)
 end do
 do l = l1+1, l2-1
   write( 3, rec=l ) -dx*(n(3)-l) + z0*(l2-l)/(l2-l1) + x(:,:,:,3)*(l-l1)/(l2-l1)
-  write( 4, rec=l )  dx*(n(3)-l) + (x(:,:,:,3)-z0)*(l2-l)/(l2-l1)
+  write( 9, rec=l )  dx*(n(3)-l) + (x(:,:,:,3)-z0)*(l2-l)/(l2-l1)
 end do
 do l = l2, n(3)
   write( 3, rec=l ) -dx*(n(3)-l) + x(:,:,:,3)
-  write( 4, rec=l )  dx*(n(3)-l) + s
+  write( 9, rec=l )  dx*(n(3)-l) + s
 end do
+close( 1 )
+close( 2 )
 close( 3 )
-close( 4 )
+close( 7 )
+close( 8 )
+close( 9 )
 
 ! Fault prestress
 deallocate( t, x, s, w )
--- versionmeta.m
+++ versionmeta.m
@@ -1,4 +1,4 @@
-version = '20060812-234622';
+version = '20060813-174149';
 login   = 'gely';
 name    = 'Geoffrey Ely';
 host    = 'wide';
