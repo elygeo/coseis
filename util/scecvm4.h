08805f49e6a685fffda22f83c632072d794ac6bb
Geoffrey Ely <gely@usc.edu>
2006-08-30 12:34:01 -0700

    .

--- util/scecvm4.h
+++
@@ -1,4 +0,0 @@
-c  in.h   contains i-o stuff
-         parameter(ibig=4000000, rdepmin=0.0)
-         common /oi/nn,rlat(ibig),rlon(ibig),rdep(ibig),
-     1   alpha(ibig),beta(ibig),rho(ibig),inout(ibig)
--- versionmeta.m
+++ versionmeta.m
@@ -1,4 +1,4 @@
-version = '20060830-104944';
+version = '20060830-123401';
 login   = 'gely';
 name    = 'Geoffrey Ely';
 host    = 'wide';
