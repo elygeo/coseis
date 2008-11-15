a24abde5bbd8a1e69f77b35845e4738f14e418bd
Geoffrey Ely <gely@usc.edu>
2008-11-14 23:21:12 -0800

    gilead

--- conf/ucb-baribu/conf.py
+++ conf/ucb-baribu/conf.py
@@ -1,8 +1,8 @@
 notes= """
-UCB cluster
+UCB Baribu cluster
 """
 login = 'baribu.geo.berkeley.edu'
 hosts = [ login ]
-nodes = 16
-cores = 4
-ram = 1500
+nodes = 7
+cores = 8
+ram = 30000
--- conf/ucb-baribu/templates/script.sh
+++ conf/ucb-baribu/templates/script.sh
@@ -1,19 +1,20 @@
 #!/bin/bash -e
 
 #PBS -N %(code)s%cound)s
-#PBS -l nodes=%(nodes):ppn=%(ppn):smallmem
+#PBS -l nodes=%(nodes):ppn=%(ppn)
 #PBS -e stderr
 #PBS -o stdout
 #PBS -m abe
 #PBS -M %(email)s
 #PBS -V
+#PBS -r n
 
 cd %(rundir)r
 
 echo "$( date ): %(code)s started" >> log
 %(pre)s
-/home/doug/bin/clean_ipc_r $( cat $PBS_NODEFILE )
-mpiexec -kill -np %(np)s %(bin)s
+export -n PBS_ENVIRONMENT
+mpirun -hostfile $PBS_NODEFILE %(bin)s
 %(post)s
 echo "$( date ): %(code)s finished" >> log
 
