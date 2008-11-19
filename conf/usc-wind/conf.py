5ba4c6229ffaa0837a1d9718d27b5ec958c9fad6
Geoffrey Ely <gely@usc.edu>
2008-11-19 15:42:27 -0800

    wind

--- conf/usc-wind/conf.py
+++ conf/usc-wind/conf.py
@@ -4,12 +4,13 @@ USC Wind
 Intel(R) Pentium(R) 4 CPU 3.20GHz
 1GB
 """
-login = 'phim.usc.edu'
+login = 'wind.usc.edu'
 hosts = [ login ]
 maxnodes = 1
 maxcores = 2
 maxram = 800
 rate = 1.0e6
+sfc = [ 'gfortran' ]
 _ = [ '-fimplicit-none', '-Wall', '-o' ]
 g = [ '-fbounds-check', '-ffpe-trap=invalid,zero,overflow', '-g' ] + _
 t = [ '-fbounds-check', '-ffpe-trap=invalid,zero,overflow' ] + _
