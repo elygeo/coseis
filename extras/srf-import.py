7e88d8ac8be9027c747dba2ef6d13515c420f31e
Geoffrey Ely <gely@usc.edu>
2008-12-09 16:07:04 -0800

    srf

--- extras/srf-import.py
+++ extras/srf-import.py
@@ -2,10 +2,10 @@
 """
 Convert SRF file to slip vectors and fault normals
 
-Reads Standard Rupture Format by Graves:
+Reads Standard Rupture Format by R. Graves:
 http://epicenter.usc.edu/cmeportal/docs/srf4.pdf
 """
-import sys, numpy
+import sys, numpy, coordinates
 
 filename = sys.argv[1]
 fh = open( filename, 'r' )
@@ -26,31 +26,41 @@ if k[0] == 'PLANE':
 if k[0] != 'POINTS':
     sys.exit( 'error' )
 nsource = int( k[1] )
-nt = []
+ntall = []
 lon = []
 lat = []
 depth = []
+nsource = 4
 for isrc in range( nsource ):
-    if isrc == 5: sys.exit()
-    lon   += [ float( k[2] ) ]
-    lat   += [ float( k[3] ) ]
-    depth += [ float( k[4] ) ]
-    strike = float( k[5] )
-    dip    = float( k[6] )
-    rake   = float( k[10] )
-    area   = float( k[7] )
-    t0     = float( k[8] )
-    dt     = float( k[9] )
-    slip   = float( k[11] ), float( k[13] ), float( k[15] )
-    nt3    = float( k[12] ), float( k[14] ), float( k[16] )
-    nt    += max( nt3 )
-    sv3    = []
-    for n in nt3
-        i = 0
-        sv = []
-        while i < n:
-            k   = fh.readline().split()
-            i  += len( k )
-            sv += [ float( k_ ) for k_ in k ]
-        sv3 += numpy.array( sv )
+    nt3 = float( k[12] ), float( k[14] ), float( k[16] )
+    nt  = nt3[0] + nt3[1] + nt3[2]
+    if nt > 0:
+        ntall += max( nt3 )
+        lon   += [ float( k[2]  ) ]
+        lat   += [ float( k[3]  ) ]
+        depth += [ float( k[4]  ) ]
+        strike =   float( k[5]  )
+        dip    =   float( k[6]  )
+        rake   =   float( k[10] )
+        area   =   float( k[7]  )
+        tm0    =   float( k[8]  )
+        dt     =   float( k[9]  )
+        slip   =   float( k[11] ), float( k[13] ), float( k[15] )
+        sv     =   []
+        while len( sv ) < nt:
+            sv += fh.readline().split()
+        if len( sv ) > nt:
+            sys.exit( 'error in sv' )
+        it = 0
+        nt = max( nt3 )
+        sv3 = numpy.zeros( ( 3, nt ) )
+        for i, n in enumerate( nt3 )
+            sv3[i,0:n] = numpy.array( sv[it:it+n] )
+            it += n
+        # rotate
+        # write
 
+t = numpy.float32
+numpy.array( ntall, dtype=t ).tofile( 'src_nt' )
+numpy.array( tm0, dtype=t ).tofile( 'src_lat' )
+numpy.array( depth, dtype=t ).tofile( 'depth' )
