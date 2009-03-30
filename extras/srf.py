946daa05a69ec1bdcd3dcf052c7f4462e766d5d5
Geoffrey Ely <gely@usc.edu>
2009-03-29 22:07:19 -0700

    srf XXX

--- extras/srf.py
+++ extras/srf.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
-"""
-Reader for Graves Standard Rupture Format (SRF):
-http://epicenter.usc.edu/cmeportal/docs/srf4.pdf
-"""
 
 def srf_read( filename, headeronly=False, noslip=False ):
     """
-    Read SRF file and return meta and data objects.
+    Reader for Graves Standard Rupture Format (SRF).
+    SRF is documented at http://epicenter.usc.edu/cmeportal/docs/srf4.pdf
+    Returns separate meta and data objects.
     Optionally include points with zero slip.
     """
     import sys, numpy
     class obj: pass
-    fh = open( filename, 'r' )
+
+    fh = filename
+    if type( fh ) is not file:
+        fh = open( fh, 'r' )
 
     # Header block
     meta = obj()
@@ -21,7 +22,7 @@ def srf_read( filename, headeronly=False, noslip=False ):
         meta.nsegments  = int( k[1] )
         k = fh.readline().split() + fh.readline().split()
         if len( k ) != 11:
-            sys.exit( 'error reading ' + filename )
+            sys.exit( 'error reading %s' % filename )
         meta.nsource2   = int(   k[2] ), int(   k[3]  )
         meta.length     = float( k[4] ), float( k[5]  )
         meta.plane      = float( k[6] ), float( k[7]  )
@@ -29,7 +30,7 @@ def srf_read( filename, headeronly=False, noslip=False ):
         meta.hypocenter = float( k[9] ), float( k[10] )
         k = fh.readline().split()
     if k[0] != 'POINTS':
-        sys.exit( 'error reading ' + filename )
+        sys.exit( 'error reading %s' % filename )
     meta.nsource = int( k[1] )
     if headeronly:
         return meta
@@ -51,7 +52,7 @@ def srf_read( filename, headeronly=False, noslip=False ):
     for isrc in range( meta.nsource ):
         k = fh.readline().split() + fh.readline().split()
         if len( k ) != 15:
-            sys.exit( 'error reading ' + filename )
+            sys.exit( 'error reading %' % filename )
         nt = int( k[10] ), int( k[12] ), int( k[14] )
         if noslip or sum( nt ) > 0:
             data.nt   += [ nt ]
@@ -69,7 +70,7 @@ def srf_read( filename, headeronly=False, noslip=False ):
             while len( sv ) < sum( nt ):
                 sv += fh.readline().split()
             if len( sv ) != sum( nt ):
-                sys.exit( 'error reading ' + filename )
+                sys.exit( 'error reading %' % filename )
             data.sv += [ float( f ) for f in sv ]
     meta.nsource = len( data.dt )
     data.nt   = numpy.array( data.nt )
@@ -90,64 +91,75 @@ def f32( a ):
     import numpy
     return numpy.array( a, numpy.float32 )
 
-def srf2potency( data, projection, path='' ): 
+def srf2potency( filename, projection, dx, path='' ): 
     """
-    Convert SRF representation to potency tensors
+    Read SRF file and write SORD potency tensor source
     """
     import os, numpy, coord
     dir = os.path.join( path, 'src_' )
+    meta, data = srf_read( filename )
+    del( meta, data.slip )
+
+    # Time history 
+    k = 0
+    for j in xrange( len( data.dt ) ):
+        for i in xrange( 3 ):
+            nt = data.nt[j,i]
+            data.sv[k:k+nt] = data.dt[j] * numpy.cumsum( data.sv[k:k+nt] )
+            k = k + nt
+    f32( data.sv ).tofile( dir + 'history' )
+    del( data.sv )
 
     # Time
+    np = data.nt.shape
     ii = data.nt > 0
     f32( data.nt )[ii].tofile( dir + 'nt' )
-    f32( data.t0 ).repeat(3)[ii].tofile( dir + 't0' )
     f32( data.dt ).repeat(3)[ii].tofile( dir + 'dt' )
+    f32( data.t0 ).repeat(3)[ii].tofile( dir + 't0' )
+    del( data.nt, data.dt, data.t0 )
 
-    # Time history
-    su = numpy.zeros_like( data.sv )
-    j = 0
-    for i in xrange( len( data.dt ) ):
-        nt = data.nt[i]
-        su[j:j+nt] = data.dt[i] * numpy.cumsum( su[j:j+nt] )
-        j = j + nt
-    f32( su ).tofile( dir + 'history' )
+    # Strike rotation
+    mat, rot = coord.rotation( data.lon, data.lat, projection )
+    data.strike = data.strike + rot
+    del( mat, rot )
+
+    # Strike, dip, and normal vectors
+    stk, dip, nrm = coord.slipvectors( data.strike, data.dip, data.rake )
+    del( data.strike, data.dip, data.rake )
 
     # Coordinates
-    x, y, z = projection( data.lon, data.lat, data.dep ):
+    x, y, z = projection( data.lon, data.lat, data.dep )
     x = x / dx[0] + 1.0
     y = y / dx[1] + 1.0
     z = z / dx[2] + 1.0
     f32( x ).repeat(3)[ii].tofile( dir + 'xi1' )
     f32( y ).repeat(3)[ii].tofile( dir + 'xi2' )
     f32( z ).repeat(3)[ii].tofile( dir + 'xi3' )
-    del( x, y, z )
-
-    # Strike dip, and rake components
-    s, d, n = coord.slipvectors( data.strike, data.dip, data.rake )
+    del( x, y, z, data.lon, data.lat, data.dep )
 
     # Normal tensor components
-    w = numpy.zeros_like( data.nt )
-    w[:,2] = data.area * n[0] * n[0]; f32( w )[ii].tofile( dir + 'w11' )
-    w[:,2] = data.area * n[1] * n[1]; f32( w )[ii].tofile( dir + 'w22' )
-    w[:,2] = data.area * n[2] * n[2]; f32( w )[ii].tofile( dir + 'w33' )
+    w = numpy.zeros( np )
+    w[:,2] = data.area * nrm[0] * n[0]; f32( w )[ii].tofile( dir + 'w11' )
+    w[:,2] = data.area * nrm[1] * n[1]; f32( w )[ii].tofile( dir + 'w22' )
+    w[:,2] = data.area * nrm[2] * n[2]; f32( w )[ii].tofile( dir + 'w33' )
 
     # Shear tensor components
-    w = numpy.zeros_like( data.nt )
-    w[:,0] = 0.5 * data.area * ( s[1] * n[2] + n[1] * s[2] )
-    w[:,1] = 0.5 * data.area * ( d[1] * n[2] + n[1] * d[2] )
+    w = numpy.zeros( np )
+    w[:,0] = 0.5 * data.area * ( stk[1] * nrm[2] + nrm[1] * stk[2] )
+    w[:,1] = 0.5 * data.area * ( dip[1] * nrm[2] + nrm[1] * dip[2] )
     f32( w )[ii].tofile( dir + 'w23' )
-    w[:,0] = 0.5 * data.area * ( s[2] * n[0] + n[2] * s[0] )
-    w[:,1] = 0.5 * data.area * ( d[2] * n[0] + d[2] * d[0] )
+    w[:,0] = 0.5 * data.area * ( stk[2] * nrm[0] + nrm[2] * stk[0] )
+    w[:,1] = 0.5 * data.area * ( dip[2] * nrm[0] + nrm[2] * dip[0] )
     f32( w )[ii].tofile( dir + 'w31' )
-    w[:,0] = 0.5 * data.area * ( s[0] * n[1] + n[0] * s[1] )
-    w[:,1] = 0.5 * data.area * ( d[0] * n[1] + n[0] * d[1] )
+    w[:,0] = 0.5 * data.area * ( stk[0] * nrm[1] + nrm[0] * stk[1] )
+    w[:,1] = 0.5 * data.area * ( dip[0] * nrm[1] + nrm[0] * dip[1] )
     f32( w )[ii].tofile( dir + 'w12' )
+    del( w, stk, dip, nrm, data.area )
 
     return
 
 if __name__ == '__main__':
-    import sys, getopt, pprint, sord
-    #opts, args = getopt.getopt( sys.argv[1:], 'aso' )
+    import sys, pprint, sord
     meta = read( sys.argv[1], true )
     pprint.pprint( sord.util.dictify( meta ) )
     
