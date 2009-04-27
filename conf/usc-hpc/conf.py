8d73e6838caf3c1601faa77ab7538f13269ddf5f
Geoffrey Ely <gely@usc.edu>
2009-04-27 10:49:07 -0700

    hpcc

--- conf/usc-hpc/conf.py
+++ conf/usc-hpc/conf.py
@@ -3,19 +3,17 @@ USC HPC
 
 http://www.usc.edu/hpcc/
 Add these to your .cshrc:
-  source /usr/usc/pgi/default/setup.csh
-  source /usr/usc/mpich/default/setup.csh
+    source /usr/usc/mpich/default/setup.csh
+    source /usr/usc/globus/default/setup.csh
 Add these to your .bashrc:
-  source /usr/usc/pgi/default/setup.sh
-  source /usr/usc/mpich/default/setup.sh
+    source /usr/usc/mpich/default/setup.sh
+    source /usr/usc/globus/default/setup.sh
 Run from disks located at HPC:
-  /auto/scec-00
-  /auto/rcf-104
-alias showme='qstat -u $USER'
-qstat -q
-pbsnodes -a | grep properties | sort | uniq -c
-qbalance -h
-showstart
+    /auto/scec-00
+    /auto/rcf-104
+I/O to temporary space:
+    /scratch
+alias showme='qstat -n | grep -E "Queue|-----|nbns"; qstat -n | grep scec'
 """
 login = 'hpc-login1.usc.edu'
 hosts = [ 'hpc-login1', 'hpc-login2' ]
@@ -25,8 +23,8 @@ queue = 'quick';    maxnodes = 4;   maxcores = 4; maxram = 3500;  maxtime = 1, 0
 queue = 'main';     maxnodes = 378; maxcores = 4; maxram = 3500;  maxtime = None
 queue = 'quick';    maxnodes = 4;   maxcores = 8; maxram = 11000; maxtime = 1, 00
 queue = 'main';     maxnodes = 382; maxcores = 8; maxram = 11000; maxtime = None
-queue = 'nbns';     maxnodes =  48; maxcores = 8; maxram = 11000; maxtime = 336, 00
 queue = 'scec';     maxnodes = 100; maxcores = 2; maxram = 1500;  maxtime = 336, 00
+queue = 'nbns';     maxnodes =  48; maxcores = 8; maxram = 11000; maxtime = 336, 00
 rate = 1.1e6
 mode = 'm'
 
