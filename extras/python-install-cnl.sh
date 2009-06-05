7ab1a76c19562aa86f5dad62e79cc3095a527fdc
Geoffrey Ely <gely@usc.edu>
2009-06-04 23:32:13 -0700

    cnl

--- extras/python-install-cnl.sh
+++ extras/python-install-cnl.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -e
-# This script installs statically linked Python.
-# Needed for Cray Compute Node Linux (CNS) on NICS Kraken.
-# http://code.google.com/p/pyprop/wiki/Installation_CrayXT4
-# http://yt.enzotools.org/wiki/CrayXT5Installation
+# This script installs statically-linked Python and Numpy
+# for Cray Compute Node Linux (CNS) on NICS Kraken.
 # https://wiki.fysik.dtu.dk/gpaw/install/Cray/louhi.html
+# http://yt.enzotools.org/wiki/CrayXT5Installation
+# http://code.google.com/p/pyprop/wiki/Installation_CrayXT4
 
 ver_python="2.6.2"
-ver_numpy="1.3.0"
-prefix="${HOME}/local"
+ver_numpy="1.2.1" # ver 1.3.0 fails for libnpymath
 prefix="/lustre/scratch/${USER}/local"
 
-echo -n "Installing Python-${ver_python} and setuptools in ${prefix}. Are you sure? [y/N]: "
+# Confirm
+echo "Installing Python-${ver_python} and Numpy-${ver_numpy} in ${prefix}."
+echo -n "Are you sure? [y/N]: "
 read confirm
 [ "$confirm" = "y" ]
 
@@ -26,7 +27,7 @@ export OPT='-O'
 
 # Fetch files
 mkdir -p "${prefix}"
-cp python-cnl.patch "${prefix}"
+cp python-cnl-*.patch "${prefix}"
 cd "${prefix}"
 wget "http://www.python.org/ftp/python/${ver_python}/Python-${ver_python}.tgz"
 wget "http://superb-west.dl.sourceforge.net/sourceforge/numpy/numpy-${ver_numpy}.tar.gz"
@@ -35,17 +36,19 @@ tar zxvf "numpy-${ver_numpy}.tar.gz"
 
 # Install Python
 cd "Python-${ver_python}"
-patch -p1 < ../python-cnl.patch
+patch -p1 < "../python-cnl-1.patch"
 ./configure --prefix="${prefix}" SO=.a DYNLOADFILE=dynload_cnl.o MACHDEP=cnl --host=x86_64-unknown-linux-gnu --disable-sockets --disable-ssl --enable-static --disable-shared
+patch -p1 < "../python-cnl-2.patch"
+#make
 make install
-#make bininstall
-#make inclinstall
-#make libainstall
 
 # Install Numpy
 cd "../numpy-${ver_numpy}"
-python setup.py install >& "numpy.install.log"
+#python setup.py build
+python setup.py install --prefix="${prefix}"
 cd "../Python-${ver_python}"
+patch -p1 < "../python-cnl-3.patch"
+#make
 make install
 
 echo "Don't forget to add \${prefix}/bin to your path"
