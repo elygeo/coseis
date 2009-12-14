128ee1822a2090b6651a12fd06b7043e166d42bd
Geoffrey Ely <gely@usc.edu>
2009-12-14 10:28:06 -0800

    dig clock

--- extras/viz.py
+++ extras/viz.py
@@ -429,6 +429,76 @@ def textpmb( x, y, s, dx=None, dy=None, fg='k', bg='w', n=16, ax=None, **kwargs 
     h += [ ax.text( x, y, s, color=fg, **kwargs ) ]
     return h
 
+class digital_clock():
+    """
+    Displays a digital clock with the format H:MM or M:SS in Mayavi.
+    Calling the digital clock object with an argument of minutes or seconds sets the time.
+    """
+    def __init__( self, x0=0, y0=0, z0=0, scale=1.0, color=(0,1,0), line_width=3 ):
+        import numpy
+        from enthought.mayavi import mlab
+        fig = mlab.gcf()
+        render = fig.scene.disable_render
+        fig.scene.disable_render = True
+        xx = x0 + scale / 200.0 * numpy.array( [
+            [  -49,  -40, numpy.nan ],
+            [   51,   60, numpy.nan ],
+            [  -60,  -51, numpy.nan ],
+            [   40,   49, numpy.nan ],
+            [  -30,   50, numpy.nan ],
+            [  -40,   40, numpy.nan ],
+            [  -50,   30, numpy.nan ],
+        ] )
+        yy = y0 + scale / 200.0 * numpy.array( [
+            [   10,   90, numpy.nan ],
+            [   10,   90, numpy.nan ],
+            [  -90,  -10, numpy.nan ],
+            [  -90,  -10, numpy.nan ],
+            [  100,  100, numpy.nan ],
+            [    0,    0, numpy.nan ],
+            [ -100, -100, numpy.nan ],
+        ] )
+        zz = z0 * numpy.ones_like( xx )
+        glyphs = [5], [0,2,4,5,6], [0,3], [0,2], [2,4,6], [1,2], [1], [0,2,5,6], [], [2]
+        hh = []
+        for g in glyphs:
+            i = numpy.array( [ i for i in range(7) if i not in g ] )
+            h = []
+            for x in -0.875, 0.125, 0.875:
+                h += [ mlab.plot3d(
+                    x + xx[i].flatten(), yy[i].flatten(), zz[i].flatten(),
+                    color=color,
+                    tube_radius=None,
+                    line_width=line_width,
+                ) ]
+            hh += [h]
+        self.glyphs = hh
+        x = x0 + scale / 200.0 * numpy.array( [-81, -79, numpy.nan, -71, -69] )
+        y = x0 + scale / 200.0 * numpy.array( [-60, -40, numpy.nan, 40, 60] )
+        z = z0 * numpy.ones_like( x )
+        self.colon = mlab.plot3d( x, y, z,
+                    color=color,
+                    tube_radius=None,
+                    line_width=line_width,
+                )
+        fig.scene.disable_render = render
+        return
+    def __call__( self, time ):
+        fig = mlab.gcf()
+        render = fig.scene.disable_render
+        fig.scene.disable_render = True
+        for hh in self.glyphs:
+            for h in hh:
+                h.visible = False
+        m = time / 60
+        d = (time % 60) / 10
+        s = time % 10
+        self.glyphs[m][0].visible = True
+        self.glyphs[d][1].visible = True
+        self.glyphs[s][2].visible = True
+        fig.scene.disable_render = render
+        return
+
 def etopo1( indices=None, path='', downsample=1, download=False ):
     """
     Download ETOPO1 Global Relief Model.
