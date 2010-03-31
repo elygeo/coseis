59a45e13b36a91a4bca8f8a66c126a969d9ad9d3
Geoffrey Ely <gely@usc.edu>
2010-03-31 16:55:14 -0700

    usc-dynamic

--- conf/usc-dynamic/conf.py
+++ conf/usc-dynamic/conf.py
@@ -1,15 +1,14 @@
 notes = """
 USC Dynamic Compute Nodes
 
-EPD version: rh3-x86_64
-
 https://geosys.usc.edu/wiki/index.php/Specifications
 https://geosys.usc.edu/wiki/index.php/MPI_on_dynamic
+https://geosys.usc.edu/wiki/index.php/PBS
 8 x 2 Dual Intel Xeon 3.2GHz
 2GB
 
 
-Not from John Yu:
+Note from John Yu:
 
 The PBS submission process now includes a routing system that will help
 prioritize jobs to the proper nodes based on resources required.  By default,
@@ -18,11 +17,20 @@ the proper node if you specify the resources needed. For example:
 
 qsub -l nodes=1,mem=20gb yourscript.pbs
 
-Please see https://geosys.usc.edu/wiki/index.php/PBS for more details.
 """
+
 login = 'dynamic.usc.edu'
 hosts = login,
 maxnodes = 12
 maxcores = 4
 maxram = 1800
+fortran_serial == 'ifort'
+fortran_mpi = 'mpif90'
+_ = '-u', '-std95', '-warn', '-o'
+fortran_flags = {
+    'g': ('-CB', '-traceback', '-g') + _,
+    't': ('-CB', '-traceback') + _,
+    'p': ('-O', '-pg') + _,
+    'O': ('-O3',) + _,
+}
 
