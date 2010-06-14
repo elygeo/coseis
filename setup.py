19f57696e943c973f12e24cee6eced2782c5c60b
Geoffrey Ely <gely@usc.edu>
2010-06-14 10:11:57 -0700

    setup

--- setup.py
+++ setup.py
@@ -2,42 +2,37 @@
 """
 Setup Coseis
 """
-import os, sys
-
+import os, sys, getopt
 if __name__ != '__main__':
-    sys.exit( 'Error, trying to import non-module: %s' % __file__ )
-
+    sys.exit( 'Error, not a module: %s' % __file__ )
 import cst
 
-def build( targets, path ):
-    if targets == []:
-        cf = cst.conf.configure( 'default', save=True )[0]
-        print( cf.notes )
-        cf = cf.__dict__
-        for k in sorted( cf.keys() ):
-            if k != 'notes':
-                print( '%s = %r' % (k, cf[k]) )
-    else:
-        for targ in targets:
-            if targ == 'build':
-                cst.tools._build()
-                cst.sord._build()
-                cst.cvm._build()
-            elif targ == 'path':
-                cst.conf.install_path( os.path.dirname( path ) )
-            elif targ == 'unpath':
-                cst.conf.uninstall_path( os.path.dirname( path ) )
-            elif targ == 'install':
-                cst.conf.install( path )
-            elif targ == 'uninstall':
-                cst.conf.uninstall( path )
-            else:
-                sys.exit( 'Unknown target' )
-    return
-
 opts, args = getopt.getopt( sys.argv[1:], '', 'machine=' )
-path = os.path.dirname( __file__ )
-path = os.path.realpath( os.path.expanduser( path ) )
-for target in sys.argv[1:]:
-    build( target, path )
+if opts:
+    machine = opts[0][1]
+    cf = cst.conf.configure( 'default', machine, save_site=True )[0]
+    print( cf.notes )
+    cf = cf.__dict__
+    for k in sorted( cf.keys() ):
+        if k != 'notes':
+            print( '%s = %r' % (k, cf[k]) )
+else:
+    path = os.path.dirname( os.path.realpath( __file__ ) )
+    for target in args:
+        if target == 'build':
+            cst.tools._build()
+            cst.sord._build()
+            cst.cvm._build()
+        elif target == 'path':
+            cst.conf.install_path( path )
+        elif target == 'unpath':
+            cst.conf.uninstall_path( path )
+        elif target == 'install':
+            path = os.path.join( path, 'cst' )
+            cst.conf.install( path )
+        elif target == 'uninstall':
+            path = os.path.join( path, 'cst' )
+            cst.conf.uninstall( path )
+        else:
+            sys.exit( 'Unknown target' )
 
