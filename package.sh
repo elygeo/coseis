66638261c6198e99587792eb553d54f882ef4625
Geoffrey Ely <gely@usc.edu>
2010-07-20 17:09:43 -0700

    package

--- package.sh
+++ package.sh
@@ -1,12 +1,10 @@
 #!/bin/bash -e
 
-cd $( dirname $0 )
-bzr shelve
-bzr version-info > version
-bzr add version
-bzr commit -m temp
-bzr export "download/coseis.tgz"
-bzr uncommit
-bzr rm --force version
-bzr unshelve
+cd "$( dirname $0 )/download"
+bzr export coseis.tar
+mkdir coseis
+bzr version-info > coseis/version
+tar -rf coseis.tar coseis/version
+gzip -c coseis.tar > coseis.tgz
+rm -r coseis.tar coseis
 
