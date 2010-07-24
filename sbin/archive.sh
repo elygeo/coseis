cf25ab72d81de2cbf984084ca77f3ec883297d85
Geoffrey Ely <gely@usc.edu>
2010-07-24 11:48:38 -0700

    archive.sh

--- sbin/archive.sh
+++ sbin/archive.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -e
 
-cd "$( dirname $( dirname $0 ) )/download"
-bzr export coseis.tar
-mkdir coseis
-bzr version-info > coseis/version
-tar -rf coseis.tar coseis/version
-gzip -c coseis.tar > coseis.tgz
-rm -r coseis.tar coseis
+cd "$( dirname $( dirname $0 ) )"
+git archive --prefix=coseis/ -o coseis.tar HEAD
+git log -100 > changelog
+tar -s,^,coseis/, -rf coseis.tar changelog
+gzip -c coseis.tar > download/coseis.tgz
+rm changelog coseis.tar
 
