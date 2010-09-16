1692ed56f1874726b2cb941c49944149f9620d50
Geoffrey Ely <gely@usc.edu>
2010-09-15 17:25:39 -0700

    fkernel

--- cst/fkernel/__init__.py
+++ cst/fkernel/__init__.py
@@ -2,12 +2,15 @@
 """
 Frechet kernel computation
 """
-import os
+import os, sys, shutil
 from ..conf import launch
 
 path = os.path.dirname( os.path.realpath( __file__ ) )
 
 def _build( optimize=None ):
+    """
+    Build code
+    """
     import cst
     cf = cst.conf.configure()[0]
     if not optimize:
@@ -33,4 +36,39 @@ def _build( optimize=None ):
     os.chdir( cwd )
     return
 
+def stage( inputs={}, **kwargs ):
+    """
+    Stage job
+    """
+    import cst
+
+    print( 'Frechet kernel setup' )
+
+    # update inputs
+    inputs = inputs.copy()
+    inputs.update( kwargs )
+
+    # configure
+    job, inputs = cst.conf.configure( **inputs )
+    if inputs:
+        sys.exit( 'Unknown parameter: %s' % inputs )
+    if not job.mode:
+        job.mode = 'm'
+    if job.mode != 'm':
+        sys.exit( 'Must be MPI' )
+    job.command = os.path.join( '.', 'cpt_ker-' + job.mode + job.optimize + ' in/input_files' )
+    job = cst.conf.prepare( job )
+
+    # build
+    if not job.prepare:
+        return job
+    _build( job.mode, job.optimize )
+
+    # create run directory
+    cst.conf.skeleton( job )
+    shutil.copytree( 'tmp', os.path.join( job.rundir, 'in' ) )
+    f = os.path.join( job.rundir, 'out' )
+    os.mkdir( f )
+
+    return job
 
