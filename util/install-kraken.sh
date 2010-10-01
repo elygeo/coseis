0f29121c2cbc3197bd9c01519879b10b2b95f977
Geoffrey Ely <gely@usc.edu>
2010-10-01 12:13:09 -0700

    kraken

--- util/install-kraken.sh
+++ util/install-kraken.sh
@@ -2,23 +2,17 @@
 # Install statically linked Python for Compute Node Linux on NICS Kraken
 # http://yt.enzotools.org/wiki/KrakenCommunityInstallation
 
-module swap PrgEnv-pgi PrgEnv-gnu
-module load git vim
-
 pwd="${PWD}"
 cd "${SCRATCHDIR}"
 
 # yt python vertion
-virtualenv --python=/lustre/scratch/proj/yt_common/2.0/bin/python local
+virtualenv --python=/lustre/scratch/proj/yt_common/trunk/bin/python local
 . local/bin/activate
 pip install pyproj
 
 # Coseis
-git clone git@github.com:gely/coseis.git
 cd coseis
 python setup.py path
-python setup.py --machine=nics-kraken
-python setup.py build
 
 cd "${pwd}"
 
