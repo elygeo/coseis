2f1c5ba9f70520c5b1b4818cb59eb7dfcf4b1231
Geoffrey Ely <gely@usc.edu>
2011-05-09 18:10:40 -0700

    .

--- scripts/kostrov/sim.py
+++ scripts/kostrov/sim.py
@@ -4,19 +4,31 @@ Kostrov circular crack test.
 """
 import cst
 
+# parameters
 nproc3 = 1, 1, 2
 delta = 100.0, 100.0, 100.0, 0.0075
 shape = 101, 101, 32, 300
+
+# boundary conditions
 bc1 = 10, 10, 10
 bc2 = -1, 1, -2
-ihypo = 1, 1, 1.5
+
+# material model
+hourglass = 1.0, 1.0
+fieldio = [
+    ('=', 'rho', [], 2670.0),
+    ('=', 'vp',  [], 6000.0),
+    ('=', 'vs',  [], 3464.0),
+    ('=', 'gam', [], 1.0),
+]
+
+# rupture parameters
 ihypo = -1, -1, -1.5
 faultnormal = 3
 vrup = 0.9 * 3464.0
 rcrit = 1e9
 trelax = 0.0
-hourglass = 1.0, 1.0
-fieldio = [
+fieldio += [
     ('=', 'rho', [], 2670.0),
     ('=', 'vp',  [], 6000.0),
     ('=', 'vs',  [], 3464.0),
@@ -26,6 +38,10 @@ fieldio = [
     ('=', 'dc',  [], 1e9),
     ('=', 'tn',  [],  -90e6),
     ('=', 'ts',  [], -100e6),
+]
+
+# receivers
+fieldio += [
     ('=w', 'svm', [ -1, -21, -1, ()], 'p20a.bin'),
     ('=w', 'svm', [-13, -17, -1, ()], 'p20b.bin'),
     ('=w', 'svm', [-17, -13, -1, ()], 'p20c.bin'),
@@ -35,5 +51,7 @@ fieldio = [
     ('=w', 'svm', [-33, -25, -1, ()], 'p40c.bin'),
     ('=w', 'svm', [-41,  -1, -1, ()], 'p40d.bin'),
 ]
+
+# launch SORD
 cst.sord.run( locals() )
 
