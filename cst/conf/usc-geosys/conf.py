29e016998736ee769aa8458578e905b541f4a29b
Geoffrey Ely <gely@usc.edu>
2011-06-30 18:12:29 -0700

    .

--- cst/conf/usc-geosys/conf.py
+++ cst/conf/usc-geosys/conf.py
@@ -1,11 +1,12 @@
 """
-USC Earth Science cluster
+USC Earth Science compute cluster
 
-alias qdev='qsub -I -l nodes=1,walltime=2:00:00'
+alias qdev='qsub -I'
+alias qme='qstat -u ${USER}'
 """
 
 login = hostname = 'geosys.usc.edu'
-maxnodes = 24
+maxnodes = 64
 maxcores = 2
 maxram = 1800
 fortran_serial = 'gfortran'
@@ -13,7 +14,7 @@ fortran_mpi = 'mpif90'
 launch = {
     's_exec':  '%(command)s',
     's_debug': 'gdb %(command)s',
-    'm_exec':  'qsub -I "%(name)s.sh"',
+    'm_exec':  'mpiexec -n %(nproc)s %(command)s',
     'submit':  'qsub "%(name)s.sh"',
     'submit2': 'qsub -W depend="afterok:%(depend)s" "%(name)s.sh"',
 }
