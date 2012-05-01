f75653ac56b2c35a30ac449b0c7fc29e9ad8a482
Geoffrey Ely <geoffely@gmail.com>
2012-05-01 10:43:49 -0500

    .

--- cst/conf/alcf-cetus/script.sh
+++ cst/conf/alcf-cetus/script.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-# {queue} {name} {nodes} {walltime} {rundir} {email}
-# qsub -t 30 -n 16 --mode script -A <your_project_name> --env BG_SHAREDMEMSIZE=32MB:PAMI_VERBOSE=1 ./myscript.sh
-
-
 cd "{rundir}"
 set > env
 
 echo "$( date ): {name} started" >> log
 {pre}
-runjob -p 16 -np 256 --block $COBALT_PARTNAME -verbose 2 --envs BG_SHAREDMEMSIZE=32MB --envs PAMI_VERBOSE=1 : {command}
+runjob -p {ppn} -n {nproc} --verbose 2 --block $COBALT_PARTNAME --envs BG_SHAREDMEMSIZE=32MB --envs PAMI_VERBOSE=1 ${COBALT_CORNER:+--corner} $COBALT_CORNER ${COBALT_SHAPE:+--shape} $COBALT_SHAPE : {command}
 {post}
 echo "$( date ): {name} finished" >> log
 
