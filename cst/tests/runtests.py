6d1d1ecf00b2461333e0483d8641e31647160623
Geoffrey Ely <geoffely@gmail.com>
2012-05-02 15:57:31 -0500

    .

--- cst/tests/runtests.py
+++ cst/tests/runtests.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 import cst
-import pprint
 
 command = "nosetests --where=%s/tests" % cst.path
 job = cst.util.prepare(command=command, seconds=600, nproc=6, rundir='.')
 job.mode = 's'
-pprint.pprint(job.__dict__)
-
-#cst.util.launch(job)
+cst.util.launch(job)
 
