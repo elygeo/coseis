7c01e69564b6ecdc0d5a28f88f1c6bef5682b6d7
Geoffrey Ely <gely@anl.gov>
2012-06-06 15:31:03 +0000

    .

--- cst/tests/test_mpi.py
+++ cst/tests/test_mpi.py
@@ -1,14 +1,14 @@
 code = """
-    program main
-    use mpi
-    integer :: e, i, n
-    call mpi_init(e)
-    call mpi_comm_rank(mpi_comm_world, i, e)
-    call mpi_comm_size(mpi_comm_world, n, e)
-    print *, 'Process ', i, ' of ', n
-    call sleep(3)
-    call mpi_finalize(e)
-    end program
+program main
+use mpi
+integer :: e, i, n
+call mpi_init(e)
+call mpi_comm_rank(mpi_comm_world, i, e)
+call mpi_comm_size(mpi_comm_world, n, e)
+print *, 'Process ', i, ' of ', n
+!call sleep(3)
+call mpi_finalize(e)
+end program
 """
 
 def test_mpi(argv=[]):
@@ -24,14 +24,14 @@ def test_mpi(argv=[]):
         nproc = 2,
         force = True,
         command = './test',
-        minutes = 10,
+        maxtime = (0, 10),
     )
     f = os.path.join(job.rundir, 'test')
     open(f + '.f90', 'w').write(code)
     c = (
         [job.fortran_mpi] +
         shlex.split(job.fortran_flags['f']) +
-        shlex.split(job.fortran_flags['t']) +
+        shlex.split(job.fortran_flags['O']) +
         ['-o', f, f + '.f90'] 
     )
     print(' '.join(c))
