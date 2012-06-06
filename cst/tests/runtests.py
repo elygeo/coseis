56a0e7e187e096761027a36afc2a71a610580c81
Geoffrey Ely <geoffely@gmail.com>
2012-06-06 13:02:58 -0500

    .

--- cst/tests/runtests.py
+++ cst/tests/runtests.py
@@ -6,6 +6,7 @@ Run all tests using Nose for test discovery.
 import os
 import cst
 
+cst.sord.build()
 path = os.path.join(cst.__path__[0], 'tests')
 cmd = "nosetests -v --where=" + path
 job = cst.util.launch(launch_command=cmd, nproc=6, seconds=600, force=True)
