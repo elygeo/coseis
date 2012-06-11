546662b3c574a368c16b5be8c7b34a4f50ae2de7
Geoffrey Ely <geoffely@gmail.com>
2012-06-10 21:17:22 -0700

    .

--- cst/conf/__init__.py
+++ cst/conf/__init__.py
@@ -5,7 +5,7 @@ Configuration modules
 import os
 f = os.path.join(os.path.dirname(__file__), 'site.py')
 if not os.path.exists(f):
-    open(f, 'a')
+    open(f, 'a').write("machine = ''\naccount = ''\nemail = ''\n")
 del(os, f)
 
 from . import site, default, cvms
