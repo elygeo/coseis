62c726a8a1f64052308afdcd1af610af3891be81
Geoffrey Ely <geoffely@gmail.com>
2012-06-16 15:30:27 -0700

    .

--- scripts/benchmark/sim.py
+++ scripts/benchmark/sim.py
@@ -36,5 +36,5 @@ for i in range(power, -1, -1):
     n = 2 ** i
     prm.nproc3 = 1, n, n
     prm.shape = points, n * points, n * points, prm.itio
-    cst.sord.run(prm, name='%s' % i, optimize='p')
+    cst.sord.run(prm, name='%s' % i, optimize='O')
 
