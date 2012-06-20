1aa83dc3d0660aaf391d9cfede6f364c64df0293
Geoffrey Ely <geoffely@gmail.com>
2012-06-20 14:33:03 -0700

    .

--- scripts/benchmark/bench.py
+++ scripts/benchmark/bench.py
@@ -18,7 +18,7 @@ prm.oplevel = 5
 prm.oplevel = 6
 prm.itstats = 1
 prm.itcheck = -1
-prm.itio = 16
+prm.itio = 128
 prm.debug = 0
 prm.delta = 100.0, 100.0, 100.0, 0.0075
 prm.bc1 = prm.bc2 = 0, 0, 0
@@ -37,5 +37,5 @@ for i in power[::-1]:
     n = 2 ** i
     prm.nproc3 = 1, n, n
     prm.shape = points, n * points, n * points, prm.itio
-    cst.sord.run(prm, name='%s' % i, minutes=20)
+    cst.sord.run(prm, name='%s' % i)
 
