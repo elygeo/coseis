4480c531ef802979d3403963330a0c1dd6c38a5c
Geoffrey Ely <geoffely@gmail.com>
2012-06-21 08:00:14 -0700

    .

--- cst/conf/alcf_bgq.py
+++ cst/conf/alcf_bgq.py
@@ -23,14 +23,14 @@ nthread = 1
 ppn_range = [1, 2, 4, 8, 16, 32]
 build_flags = '-g -O3'
 build_libs = '-lSPI_upci_cnk /home/morozov/fixes/libc.a /home/morozov/HPM/lib/libmpihpm.a /bgsys/drivers/ppcfloor/bgpm/lib/libbgpm.a'
-launch = 'runjob -n {nproc} -p {ppn} --verbose=INFO --block $COBALT_PARTNAME --envs BG_SHAREDMEMSIZE=32MB PAMID_VERBOSE=1 ${{COBALT_CORNER:+--corner}} $COBALT_CORNER ${{COBALT_SHAPE:+--shape}} $COBALT_SHAPE VPROF_PROFILE=yes : {command}\n'
+launch = 'runjob --exe {command} -n {nproc} -p {ppn} --verbose=INFO --block $COBALT_PARTNAME --envs BG_SHAREDMEMSIZE=32MB PAMID_VERBOSE=1 ${{COBALT_CORNER:+--corner}} $COBALT_CORNER ${{COBALT_SHAPE:+--shape}} $COBALT_SHAPE VPROF_PROFILE=yes\n'
 
 # MPI + OpenMP
 nthread = 32
 ppn_range = [1]
 build_flags = '-g -O3 -qsmp=omp'
 build_libs = '-lSPI_upci_cnk /home/morozov/fixes/libc.a /home/morozov/HPM/lib/libmpihpm_smp.a /bgsys/drivers/ppcfloor/bgpm/lib/libbgpm.a'
-launch = 'runjob -n {nproc} -p {ppn} --verbose=INFO --block $COBALT_PARTNAME --envs BG_SHAREDMEMSIZE=32MB PAMID_VERBOSE=1 ${{COBALT_CORNER:+--corner}} $COBALT_CORNER ${{COBALT_SHAPE:+--shape}} $COBALT_SHAPE OMP_NUM_THREADS={nthread} VPROF_PROFILE=yes : {command}\n'
+launch = 'runjob --exe {command} -n {nproc} -p {ppn} --verbose=INFO --block $COBALT_PARTNAME --envs BG_SHAREDMEMSIZE=32MB PAMID_VERBOSE=1 ${{COBALT_CORNER:+--corner}} $COBALT_CORNER ${{COBALT_SHAPE:+--shape}} $COBALT_SHAPE VPROF_PROFILE=yes OMP_NUM_THREADS={nthread}\n'
 
 # compiler commands
 build_cc = 'mpixlcc_r -qlist -qreport -qsuppress=cmpmsg'
@@ -42,5 +42,6 @@ build_real8 = '-qrealsize=8'
 
 # job submission
 submit =  'qsub -O {name} -A {account} -n {nodes} -t {minutes} --mode script --env BG_SHAREDMEMSIZE=32MB:PAMID_VERBOSE=1:VPROF_PROFILE=yes "{name}.sh"'
+submit =  'qsub "{name}.sh" -O {name} -A {account} -n {nodes} -t {minutes} --mode script --env BG_SHAREDMEMSIZE=32MB:PAMID_VERBOSE=1:VPROF_PROFILE=yes "{name}.sh"'
 submit2 = 'qsub -O {name} -A {account} -n {nodes} -t {minutes} --mode script --env BG_SHAREDMEMSIZE=32MB:PAMID_VERBOSE=1:VPROF_PROFILE=yes --dependenices {depend} "{name}.sh"'
 
