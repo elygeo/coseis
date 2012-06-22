d542868587d84e0f3b6838d75129020fa3f9a247
Geoffrey Ely <geoffely@gmail.com>
2012-06-22 12:14:42 -0700

    .

--- cst/conf/__init__.py
+++ cst/conf/__init__.py
@@ -6,7 +6,7 @@ import os
 f = os.path.join(os.path.dirname(__file__), 'site.py')
 if not os.path.exists(f):
     open(f, 'a').write('')
-del(os, socket, f)
+del(os, f)
 
 from . import site, default, cvms
 site, default, cvms
