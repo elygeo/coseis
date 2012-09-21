4e79eb8b8c11e8f758cac92750ba896a05175568
Geoffrey Ely <geoffely@gmail.com>
2012-09-21 11:19:25 +0200

    .

--- cst/conf/kaust_shaheen.py
+++ cst/conf/kaust_shaheen.py
@@ -1,18 +1,22 @@
 """
 KAUST Shaheen: IBM Blue Gene/P
 
-module load GNU
+module unload GNU
+module load IBM
 module load numpy
 """
 
 maxcores = 4
 maxram = 4096
 ppn_range = [1, 2, 4]
+nthread = 1
+build_ldflags = '-g -O5 -qarch=450d -qtune=450 -qsuppress=cmpmsg'
 
-account = 'k33'
+account = 'k68'
 queue = 'default'
 queue_opts = [
     ('development', {'maxnodes':  8 * 1024, 'maxtime':   30}),
+    ('smallblocks', {'maxnodes':      1024, 'maxtime': 1440}),
     ('pset64',      {'maxnodes':  4 * 1024, 'maxtime': 1440}),
     ('pset128',     {'maxnodes': 12 * 1024, 'maxtime': 1440}),
     ('default',     {'maxnodes': 16 * 1024, 'maxtime': 1440}),
