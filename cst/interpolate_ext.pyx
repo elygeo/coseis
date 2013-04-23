2297b9868cba4e66b6f3775b78e22c1af132c191
Geoffrey Ely <geoffely@gmail.com>
2013-04-23 13:58:22 -0700

    .

---
+++ cst/interpolate_ext.pyx
@@ -0,0 +1,115 @@
+"""
+Interpolation functions
+"""
+
+#cython: boundscheck=False
+#cython: wraparound=False
+#cython: cdivision=True
+
+import numpy as np
+cimport numpy as np
+
+def trinterp(x, f, t, xi, fi=None, no_data_val=float('nan')):
+    """
+    2D linear interpolation of function values specified on triangular mesh.
+
+    Parameters
+    ----------
+    x:  shape (2, M) array of vertex coordinates.
+    f:  shape (M) array of function values at the vertices.
+    t:  shape (3, N) array of vertex indices for the triangles.
+    xi: shape (2, ...) array of coordinates for the interpolation points.
+
+    Returns
+    -------
+    fi: Array of interpolated values, same shape as `xi[0]`.
+    """
+
+    # declarations
+    cdef np.ndarray[double, ndim=1, mode='c'] x_, y_, f_, xi_, yi_, fi_
+    cdef np.ndarray[int, ndim=1, mode='c'] i0_, i1_, i2_
+    cdef double A00, A01, A10, A11, b0, b1, d
+    cdef double l0, l1, l2, lmin, lmax
+    cdef int i0, i1, i2, i, j, k, m, n
+
+    # tolerance
+    lmin = -0.000001
+    lmax =  1.000001
+
+    # input arrays
+    x_  = np.ascontiguousarray(x[0], 'd')
+    y_  = np.ascontiguousarray(x[1], 'd')
+    f_  = np.ascontiguousarray(f, 'd')
+    i0_ = np.ascontiguousarray(t[0], 'i')
+    i1_ = np.ascontiguousarray(t[1], 'i')
+    i2_ = np.ascontiguousarray(t[2], 'i')
+    xi_ = np.ascontiguousarray(xi[0], 'd').reshape(-1)
+    yi_ = np.ascontiguousarray(xi[1], 'd').reshape(-1)
+
+    # output array
+    if fi == None:
+        fi_ = np.empty_like(xi_)
+        fi_.fill(no_data_val)
+    else:
+        if fi.dtpe.char != 'd':
+            raise ValueError('`fi` must be type double')
+        if not fi.flags.contiguous:
+            raise ValueError('`fi` must be contiguous')
+        fi_ = fi.reshape(-1)
+
+    # bounds check
+    m = x_.shape[0]
+    if any(i0_ >= m) or any(i1_ >= m) or any(i2_ >= m):
+        raise ValueError('`t` indices out of bounds')
+
+    # loop over interpolation points, then triangles
+    k = 0
+    m = xi_.shape[0]
+    n = i0_.shape[0]
+    for i in range(m):
+        for j in range(n):
+
+            # barycentric coordinates
+            i0 = i0_[k]
+            i1 = i1_[k]
+            i2 = i2_[k]
+            A00 = x_[i1] - x_[i0]
+            A01 = x_[i2] - x_[i0]
+            A10 = y_[i1] - y_[i0]
+            A11 = y_[i2] - y_[i0]
+            d = A00 * A11 - A01 * A10
+
+            # if zero area triangle, move to the next one
+            if d == 0.0:
+                print('Degenerate triange: %s of %s' % (k, n))
+                if j % 2:
+                    k = (n + k - j - 1) % n
+                else:
+                    k = (n + k + j + 1) % n
+                continue
+
+            d = 1.0 / d
+            b0 = xi_[i] - x_[i0]
+            b1 = yi_[i] - y_[i0]
+            l1 = d * A11 * b0 - d * A01 * b1
+            l2 = d * A00 * b1 - d * A10 * b0
+            l0 = 1.0 - l1 - l2
+
+            # if outside the triangle, move to the next one
+            if (
+               (l1 < lmin) | (l1 > lmax) |
+               (l2 < lmin) | (l2 > lmax) |
+               (l0 < lmin) | (l0 > lmax)
+            ):
+                if j % 2:
+                    k = (n + k - j - 1) % n
+                else:
+                    k = (n + k + j + 1) % n
+                continue
+
+            # evaluate function
+            fi_[i] = l0 * f_[i0] + l1 * f_[i1] + l2 * f_[i2]
+            break
+
+    return fi_.reshape(xi[0].shape)
+
---
+++ cst/signal_ext.f90
@@ -0,0 +1,121 @@
+! Response Spectra
+!
+! Original code by Doug Dreger
+! Wrapper by Geoffrey Ely
+!
+! Create Python module with:
+!     f2py -c -m rspectra rspectra.f90
+!
+! real component of displacement spectrum:    rd = z(1)
+! real component of velocity spectrum:        rv = z(2)
+! real component of acceleration spectrum:    aa = z(3)
+! imaginary component of velocity spectrum:   prv = w * z(1)
+! imaginary component of velocity spectrum:   pra = w * prv
+! imaginary vel = i*w* real displacement
+! imaginary acc = i*w* real velocity
+
+! Wrapper
+! u  : acceleration time history
+! dt : time step length
+! w  : angular frequency = 2 pi / T
+! d  : damping ratio
+! n  : number of samples (automatically determined)
+subroutine rspectra( z, u, dt, w, d, n )
+implicit none
+real, intent(out) :: z(3)
+real, intent(in) :: u(n), dt, w, d
+integer, intent(in) :: n
+!f2py intent(hide) :: n
+if ( w > 0.4 * acos(0.0) / dt ) then
+    call ucmpmx( z, u, dt, w, d, n-1 )
+else
+    call cmpmax( z, u, dt, w, d, n-1 )
+end if
+end subroutine
+
+subroutine cmpmax(z, ug, dt, w, d, kug)
+real ug(*), x(2,3), z(3), c(3)
+wd = sqrt(1.0 - d * d) * w
+w2 = w * w
+w3 = w2 * w
+do i = 1, 3
+    x(1,i) = 0.0
+    z(i) = 0.0
+end do
+f1 = 2.0 * d / (w3 * dt)
+f2 = 1.0 / w2
+f3 = d * w
+f4 = 1.0 / wd
+f5 = f3 * f4
+f6 = 2.0 * f3
+e = exp(-f3 * dt)
+g1 = e * sin(wd * dt)
+g2 = e * cos(wd * dt)
+h1 = wd * g2 - f3 * g1
+h2 = wd * g1 + f3 * g2
+do k = 1, kug
+    dug = ug(k+1) - ug(k)
+    z1 = f2 * dug
+    z2 = f2 * ug(k)
+    z3 = f1 * dug
+    z4 = z1 / dt
+    b = x(1,1) + z2 - z3
+    a = f4 * x(1,2) + f5 * b + f4 * z4
+    x(2,1) = a * g1 + b * g2 + z3 - z2 - z1
+    x(2,2) = a * h1 - b * h2 - z4
+    x(2,3) = -f6 * x(2,2) - w2 * x(2,1)
+    do l = 1, 3
+        c(l) = abs(x(2,l))
+        if (c(l) .gt. z(l)) z(l) = c(l)
+        x(1,l) = x(2,l)
+    end do
+end do
+end subroutine
+
+subroutine ucmpmx(z, ug, dt0, w, d, kug)
+real ug(*), z(3), c(3), x(2,3)
+pr = 4.0 * acos(0.0) / w
+wd = sqrt(1.0 - d * d) * w
+w2 = w * w
+w3 = w2 * w
+do i = 1, 3
+    x(1,i) = 0.0
+    z(i) = 0.0
+end do
+f2 = 1.0 / w2
+f3 = d * w
+f4 = 1.0 / wd
+f5 = f3 * f4
+f6 = 2.0 * f3
+dt = dt0
+ns = nint(10.0 * dt / pr)+1
+dt = dt / real(ns)
+f1 = 2.0 * d / w3 / dt
+e = exp(-f3 * dt)
+g1 = e * sin(wd * dt)
+g2 = e * cos(wd * dt)
+h1 = wd * g2 - f3 * g1
+h2 = wd * g1 + f3 * g2
+do k = 1, kug
+    dug = (ug(k+1) - ug(k)) / real(ns)
+    g = ug(k)
+    z1 = f2 * dug
+    z3 = f1 * dug
+    z4 = z1 / dt
+    do is = 1, ns
+        z2 = f2 * g
+        b = x(1,1) + z2 - z3
+        a = f4 * x(1,2) + f5 * b + f4 * z4
+        x(2,1) = a * g1 + b * g2 + z3 - z2 - z1
+        x(2,2) = a * h1 - b * h2 - z4
+        x(2,3) = -f6*x(2,2) - w2 * x(2,1)
+        do l = 1, 3
+            c(l) = abs(x(2,l))
+            if (c(l) .gt. z(l)) z(l) = c(l)
+            x(1,l) = x(2,l)
+        end do
+        g = g + dug
+    end do
+end do
+end subroutine
+
