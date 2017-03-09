7f4ad8dd4330f6a1005bf67dcd9bd25e3732a988
Geoffrey Ely <geoffely@gmail.com>
2017-03-08 16:03:38 -0800

    .

--- cst/bin2npy.py
+++ cst/bin2npy.py
@@ -1,7 +1,5 @@
 """
 Convert binary files to NumPy .npy format.
-
-Data types will be read from file 'meta.json' if it exists.
 """
 import os
 import sys
@@ -9,48 +7,29 @@ import json
 import numpy
 
 
-def convert(filenames, dtype='f', shape=[]):
-    for fn in filenames:
-        t = dtype
-        n = shape
-        p = ['.'] + os.path.split(fn)
-        while p:
-            f = os.sep.join(p + ['meta.json'])
-            g = os.sep.join(p + ['meta', 'meta.json'])
-            if os.path.exists(f):
-                m = json.load(open(f))
-            elif os.path.exists(g):
-                m = json.load(open(g))
-            else:
-                continue
-            if 'dtype' in m:
-                t = m['dtype']
-            if 'shapes' in m and fn in m['shapes']:
-                n = m['shape'][fn]
-            elif 'shape' in m:
-                n = m['shape']
-            break
-        if fn.endswith('.bin'):
-            f = fn[:-4] + '.npy'
+def convert(filenames, **metadata):
+    meta = {'dtype': 'f', 'shape': []}
+    meta.update(metadata)
+    for f in filenames:
+        if f.endswith('.json'):
+            meta.update(json.load(open(f)))
+        dtype = meta['dtype']
+        if 'shapes' in meta and f in meta['shapes']:
+            shape = meta['shapes'][f]
         else:
-            f = fn + '.npy'
-        if os.path.exists(f):
+            shape = meta['shape']
+        if f.endswith('.bin'):
+            g = f[:-4] + '.npy'
+        else:
+            g = f + '.npy'
+        if os.path.exists(g):
             return
-        x = numpy.fromfile(fn, t)
-        if n:
-            x = x.reshape(n[::-1]).T
-        print('%s: %s %s' % (fn, t, x.shape))
+        x = numpy.fromfile(f, dtype)
+        if shape:
+            x = x.reshape(shape[::-1]).T
+        print('%s: %s %s' % (g, dtype, x.shape))
         numpy.save(g, x)
 
 
 if __name__ == '__main__':
-    args = []
-    kw = {}
-    for i in sys.argv[1:]:
-        if i[:1] == '-':
-            kw['dtype'] = i.lstrip('-')
-        elif i[:1] + i[-1:] == '[]':
-            kw['shape'] = json.loads(i)
-        else:
-            args.append(i)
-    convert(args, **kw)
+    convert(sys.argv[1:])
