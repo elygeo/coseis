ad0a7264fbd8a61f1e49152c12e1620ba785b011
Geoffrey Ely <geoffely@gmail.com>
2017-04-02 12:42:32 -0700

    .

--- cst/setup.py
+++ cst/setup.py
@@ -1,24 +1,32 @@
 import os
+import sys
 import site
 from . import home
 
 
-def set_path():
+def set_path(force=False):
+    home0 = home
     d = site.USER_SITE + os.sep
     f = d + __package__ + '.pth'
-    print('Package home: ' + home)
     if os.path.exists(f):
-        home1 = open(f).read()
-        print('Path file: ' + f)
+        home1 = open(f).read().strip()
         if home != home1:
-            print('with different path ' + home1)
+            if force:
+                print('Updating path file')
+                open(f, 'w').write(home)
+            else:
+                print('Path file exists, use -f to force update')
+                home0 = home1
+        else:
+            print('Path file is up to date')
     else:
+        print('Creating path file')
         if not os.path.exists(d):
-            print('Creating ' + d)
             os.makedirs(d)
-        print('Creating path file ' + f)
         open(f, 'w').write(home)
+    print(f)
+    print(home0)
 
 
 if __name__ == '__main__':
-    set_path()
+    set_path('-f' in sys.argv[1:])
